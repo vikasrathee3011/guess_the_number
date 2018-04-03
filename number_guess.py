a770e30d138d7f0d2c927f35c86b8f01f71ac5a8
vikas rathee <vikasrathee3011@gmail.com>
2018-04-03 19:48:22 +0530

    Update number_guess.py

--- number_guess.py
+++ number_guess.py
@@ -1,12 +1,16 @@
 import math
-def guess():
-    A, B = map(int, input('enter the lower bound and upper bound respectively of your number\n').split(" "))
-    N = int(input('number of guesses in which you wanted to guess your number\n'))
+#loadinf math module
+
+def guess():                                                                                                      #defining function          
+    A, B = map(int, input('enter the lower bound and upper bound respectively of your number\n').split(" "))        #taking inuputs for upper and lower bound at the same time
+    N = int(input('number of guesses in which you wanted to guess your number\n'))                                  #inputing the number of guess
     i = 1
+
     while i <= N:
         d = math.ceil((A + B)/2)
         print('is your number is this',d)
-        ans = str(input('enter your choice as TOO_BIG, TOO_SMALL or CORRECT\n'))
+        ans = str(input('enter your choice as TOO_BIG, TOO_SMALL or CORRECT\n'))                                    #enter the choice weather the number displayed is big, small or wrong
+        
         if ans == 'TOO_SMALL':
             A = int(d)
         elif ans == 'TOO_BIG':
@@ -17,6 +21,7 @@ def guess():
             print('WRONG_ANSWER, please do it again')
             break
         i = i + 1
+    
     print('perhaps, we are not able to get the correct number in this limited number of gusses but still we has narrowed you range\n your number lies between',A, B)
 
 guess()
